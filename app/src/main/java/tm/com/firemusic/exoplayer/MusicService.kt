5a05290a1413ccdb339baa5ec9b295432461eb9f
Merdan <egemberdiyevmerdan@gmail.com>
2021-11-27 20:33:50 +0500

    music service

---
+++ app/src/main/java/tm/com/firemusic/exoplayer/MusicService.kt
@@ -0,0 +1,71 @@
+package tm.com.firemusic.exoplayer
+
+import android.app.PendingIntent
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.media.MediaBrowserServiceCompat
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import javax.inject.Inject
+
+private const val SERVICE_TAG = "MusicService"
+
+@AndroidEntryPoint
+class MusicService : MediaBrowserServiceCompat(){
+
+    @Inject
+    lateinit var dataSource : DefaultDataSource
+
+    @Inject
+    lateinit var exoPlayer: ExoPlayer
+
+    private val serviceJob = Job()
+    private val serviceScope  = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    private lateinit var mediaSession : MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this,0,it,0)
+        }
+
+        mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
+            setSessionActivity(activityIntent)
+            isActive = true
+        }
+
+        sessionToken = mediaSession.sessionToken
+
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector.setPlayer(exoPlayer)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
+    }
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+
+    }
+
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/tm/com/firemusic/ui/MainActivity.kt
+++ app/src/main/java/tm/com/firemusic/ui/MainActivity.kt
@@ -2,8 +2,10 @@ package tm.com.firemusic.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import tm.com.firemusic.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
